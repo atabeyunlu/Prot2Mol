c7a6f13415f7eef0a6fc9117d71f4e7201b784e7
afurkaan <141612173+afurkaan@users.noreply.github.com>
2024-11-16 05:52:24 +0100

    Update data_loader.py

--- prot2mol/data_loader.py
+++ prot2mol/data_loader.py
@@ -4,6 +4,7 @@ import h5py
 
 class CustomDataset(Dataset):
     def __init__(self, ligand_data, target_data, tokenizer, max_length=200):
+        #now target data is protein sequences
         self.ligand_data = ligand_data
         self.tokenizer = tokenizer
         self.max_length = max_length
@@ -11,7 +12,11 @@ class CustomDataset(Dataset):
         self.length = len(self.ligand_data)
         
     def preprocess_target_data(self, target_data):
-        return {target_id: enc_state for target_id, enc_state in zip(target_data["Target_CHEMBL_ID"], target_data["encoder_hidden_states"])}
+        #return {target_id: enc_state for target_id, enc_state in zip(target_data["Target_CHEMBL_ID"], target_data["encoder_hidden_states"])}
+        return {target_id: prot_seq for target_id, prot_seq in zip(target_data["Target_CHEMBL_ID"], target_data["Target_FASTA"])}
+        #!!!!CHECK THE TARGET DATA, WHAT IS THAT ACTUALLY SINCE IT SUPPOSE TO INCLUDE BOTH CHEMBL_ID FASTA AND EMBEDDINGS
+        #also where is ligand data come from, it supposed to be the part of the papyrus data
+        #Is that needed to save both of these???
 
     def __len__(self):
         return self.length
@@ -28,26 +33,33 @@ class CustomDataset(Dataset):
     def _get_single(self, idx):
         ligand_row = self.ligand_data.iloc[idx]
         target_id = ligand_row["Target_CHEMBL_ID"]
-        enc_state = self.target_data_dict[target_id]
+        #enc_state = self.target_data_dict[target_id]
+        prot_seq = self.target_data_dict[target_id]
         compound_selfies = ligand_row["Compound_SELFIES"]
         input_ids = self.get_input_ids(compound_selfies)
-        return {"input_ids": input_ids, "encoder_hidden_states": enc_state}
+        #return {"input_ids": input_ids, "encoder_hidden_states": enc_state}
+        return {"input_ids": input_ids, "protein_sequences": prot_seq}
 
     def _get_batch(self, indices):
         batch_size = len(indices)
         sample = self._get_single(indices[0])
         input_ids_shape = (batch_size,) + sample["input_ids"].shape
-        encoder_hidden_states_shape = (batch_size,) + sample["encoder_hidden_states"].shape
+        #encoder_hidden_states_shape = (batch_size,) + sample["encoder_hidden_states"].shape
+        protein_sequences_shape = (batch_size,) + sample["protein_sequences"].shape
+
 
         input_ids_batch = torch.empty(input_ids_shape, dtype=torch.long)
-        encoder_hidden_states_batch = torch.empty(encoder_hidden_states_shape, dtype=torch.float)
+        #encoder_hidden_states_batch = torch.empty(encoder_hidden_states_shape, dtype=torch.float)
+        protein_sequences_batch = torch.empty(protein_sequences_shape, dtype=torch.long)
 
         for i, idx in enumerate(indices):
             sample = self._get_single(idx)
             input_ids_batch[i] = sample["input_ids"]
-            encoder_hidden_states_batch[i] = sample["encoder_hidden_states"]
+            #encoder_hidden_states_batch[i] = sample["encoder_hidden_states"]
+            protein_sequences_batch[i] = sample["protein_sequences"]
 
-        return {"input_ids": input_ids_batch, "encoder_hidden_states": encoder_hidden_states_batch}
+        #return {"input_ids": input_ids_batch, "encoder_hidden_states": encoder_hidden_states_batch}
+        return {"input_ids": input_ids_batch, "protein_sequences": protein_sequences_batch}
 
     def get_input_ids(self, compound_selfies):
         return self.tokenizer(compound_selfies, 
